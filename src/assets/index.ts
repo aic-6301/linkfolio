48891d0386bd3d2c52e0fe21d59fd48c4c043cb5
aic-6301 <rx435423@gmail.com>
2024-06-17 09:36:13 +0900

    Profile Edit

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import linkfolioIcon from "./img/linkfolio_logo.svg";
 import defaultAvatarIcon from "./img/avatar.webp";
+import discordIcon from "./img/discord.webp";
 import blueSkyIcon from "./img/bluesky.webp";
 import facebookIcon from "./img/facebook.webp";
 import githubIcon from "./img/github.webp";
@@ -7,14 +8,17 @@ import instagramIcon from "./img/instagram.webp";
 import linkedinIcon from "./img/linkedin.webp";
 import snapchatIcon from "./img/snapchat.webp";
 import mastodonIcon from "./img/mastodon.webp";
+import misskeyIcon from "./img/misskey.webp";
 import telegramIcon from "./img/telegram.webp";
 import threadsIcon from "./img/threads.webp";
 import whatsappIcon from "./img/whatsapp.webp";
 import youtubeIcon from "./img/youtube.webp";
 import xIcon from "./img/x.webp";
+import rocket from "./img/rocket.webp";
 
 export {
   linkfolioIcon,
+  discordIcon,
   defaultAvatarIcon,
   blueSkyIcon,
   facebookIcon,
@@ -22,10 +26,12 @@ export {
   instagramIcon,
   linkedinIcon,
   mastodonIcon,
+  misskeyIcon,
   snapchatIcon,
   telegramIcon,
   threadsIcon,
   whatsappIcon,
   youtubeIcon,
   xIcon,
+  rocket,
 };
