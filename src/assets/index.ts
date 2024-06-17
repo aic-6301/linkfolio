d45a9577a802b57b39a38be4a86c6644c9bbd117
aic-6301 <rx435423@gmail.com>
2024-06-17 09:49:59 +0900

    importしすぎ

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,16 +2,8 @@ import linkfolioIcon from "./img/linkfolio_logo.svg";
 import defaultAvatarIcon from "./img/avatar.webp";
 import discordIcon from "./img/discord.webp";
 import blueSkyIcon from "./img/bluesky.webp";
-import facebookIcon from "./img/facebook.webp";
 import githubIcon from "./img/github.webp";
-import instagramIcon from "./img/instagram.webp";
-import linkedinIcon from "./img/linkedin.webp";
-import snapchatIcon from "./img/snapchat.webp";
-import mastodonIcon from "./img/mastodon.webp";
 import misskeyIcon from "./img/misskey.webp";
-import telegramIcon from "./img/telegram.webp";
-import threadsIcon from "./img/threads.webp";
-import whatsappIcon from "./img/whatsapp.webp";
 import youtubeIcon from "./img/youtube.webp";
 import xIcon from "./img/x.webp";
 import rocket from "./img/rocket.webp";
@@ -21,16 +13,8 @@ export {
   discordIcon,
   defaultAvatarIcon,
   blueSkyIcon,
-  facebookIcon,
   githubIcon,
-  instagramIcon,
-  linkedinIcon,
-  mastodonIcon,
   misskeyIcon,
-  snapchatIcon,
-  telegramIcon,
-  threadsIcon,
-  whatsappIcon,
   youtubeIcon,
   xIcon,
   rocket,
